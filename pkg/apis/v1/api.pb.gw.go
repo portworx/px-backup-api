d16351d7bc90201e2b808cd41d100f20ac7457f6
Abhisheka <aks@purestorage.com>
2025-03-19 05:18:52 +0000

    PB-9611:Add GetKeysFromKMS API
    Signed-off-by: Abhisheka <aks@purestorage.com>

--- pkg/apis/v1/api.pb.gw.go
+++ pkg/apis/v1/api.pb.gw.go
@@ -674,6 +674,23 @@ func request_KmsCredential_GetReferencedCount_0(ctx context.Context, marshaler r
 }
 
 var (
+	filter_KmsCredential_GetKeysFromKMS_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_KmsCredential_GetKeysFromKMS_0(ctx context.Context, marshaler runtime.Marshaler, client KmsCredentialClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetKeysFromKMSRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_KmsCredential_GetKeysFromKMS_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetKeysFromKMS(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+var (
 	filter_KmsCredential_Enumerate_0 = &utilities.DoubleArray{Encoding: map[string]int{"org_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
@@ -3553,6 +3570,26 @@ func RegisterKmsCredentialHandlerClient(ctx context.Context, mux *runtime.ServeM
 
 	})
 
+	mux.Handle("GET", pattern_KmsCredential_GetKeysFromKMS_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_KmsCredential_GetKeysFromKMS_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_KmsCredential_GetKeysFromKMS_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_KmsCredential_Enumerate_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -3685,6 +3722,8 @@ var (
 
 	pattern_KmsCredential_GetReferencedCount_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "kmscredential", "getreferencedcount"}, ""))
 
+	pattern_KmsCredential_GetKeysFromKMS_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "kmscredential", "getkeysfromkms"}, ""))
+
 	pattern_KmsCredential_Enumerate_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "kmscredential", "org_id"}, ""))
 
 	pattern_KmsCredential_Inspect_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 1, 0, 4, 1, 5, 3, 1, 0, 4, 1, 5, 4}, []string{"v1", "kmscredential", "org_id", "name", "uid"}, ""))
@@ -3706,6 +3745,8 @@ var (
 	forward_KmsCredential_GetReferencedKMSObject_0 = runtime.ForwardResponseMessage
 
 	forward_KmsCredential_GetReferencedCount_0 = runtime.ForwardResponseMessage
+
+	forward_KmsCredential_GetKeysFromKMS_0 = runtime.ForwardResponseMessage
 
 	forward_KmsCredential_Enumerate_0 = runtime.ForwardResponseMessage
 
