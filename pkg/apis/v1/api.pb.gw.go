a3805e9f4d0176dbcebf6bf963e9e782b3a0b257
Lalatendu Das <ldas@purestorage.com>
2025-09-01 17:55:00 +0000

    pb-11913: Add chnages to support granular restore
    This proto changes alters the GetBackupResourceDetails API
    to be used for granular restore operation.
    
    Signed-off-by: Lalatendu Das <ldas@purestorage.com>
    Signed-off-by: Kesavan Thiruvenkadasamy <kthiruvenkadasamy@purestorage.com>

--- pkg/apis/v1/api.pb.gw.go
+++ pkg/apis/v1/api.pb.gw.go
@@ -3867,6 +3867,10 @@ func local_request_Backup_UpdateBackupShare_0(ctx context.Context, marshaler run
 
 }
 
+var (
+	filter_Backup_GetBackupResourceDetails_0 = &utilities.DoubleArray{Encoding: map[string]int{"org_id": 0, "name": 1, "uid": 2}, Base: []int{1, 1, 2, 3, 0, 0, 0}, Check: []int{0, 1, 1, 1, 2, 3, 4}}
+)
+
 func request_Backup_GetBackupResourceDetails_0(ctx context.Context, marshaler runtime.Marshaler, client BackupClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq BackupResourceDetailGetRequest
 	var metadata runtime.ServerMetadata
@@ -3909,6 +3913,13 @@ func request_Backup_GetBackupResourceDetails_0(ctx context.Context, marshaler ru
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "uid", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Backup_GetBackupResourceDetails_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetBackupResourceDetails(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -3958,6 +3969,13 @@ func local_request_Backup_GetBackupResourceDetails_0(ctx context.Context, marsha
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "uid", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Backup_GetBackupResourceDetails_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetBackupResourceDetails(ctx, &protoReq)
